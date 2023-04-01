96611364c1b635e87a43a4c7d76c274b6e55cbb7
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-04-01 10:25:03 +0200

    improve user experience

--- src/ia.rs
+++ src/ia.rs
@@ -1,9 +1,38 @@
+use hyper::Request;
 use serde::{Serialize, Deserialize};
 use tokio::fs::read_to_string;
 use std::{process, path::PathBuf};
 use serde_json::json;
-use dirs::config_dir;
 use tracing::{info, debug, error};
+use std::error::Error;
+use std::fmt;
+
+use hyper::Client;
+
+#[derive(Debug)]
+pub struct IABotError{
+    message: String,
+}
+
+impl IABotError{
+    pub fn new(message: &str) -> Self{
+        Self {
+            message: message.into()
+        }
+    }
+}
+
+
+// Display implementation is required for std::error::Error.
+impl fmt::Display for IABotError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        //write!(f, "Address is localhost")
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for IABotError {} // Defaul
+
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct IABot{
@@ -31,7 +60,7 @@ fn get_default_log_level() -> String{
 }
 
 fn get_default_base_url() -> String {
-    return "https://api.openai.com".to_string();
+    return "api.openai.com".to_string();
 }
 
 fn get_default_endpoint() -> String {
@@ -55,92 +84,87 @@ fn get_default_temperature() -> String {
 }
 
 fn get_default_content() -> String {
-    return "Eres un asistente útil. Generarás comandos '$SHELL' en función de la entrada del usuario. Tu respuesta debe contener SOLO el comando y NO una explicación. NUNCA uses saltos de línea para separar comandos, en su lugar usa ; o &&. El directorio de trabajo actual es '$PWD'.".to_string();
+    return "Eres un asistente útil. Generarás comandos '$SHELL' en función de la entrada del usuario. Tu respuesta debe contener SOLO el comando y NO una explicación. NUNCA uses saltos de línea para separar comandos, en su lugar usa ; o &&. El directorio de trabajo actual es '$PWD'".to_string();
 }
 
+
 impl IABot{
-
-    pub async fn new() -> Self{
-        let mut config_file = PathBuf::new();
-        match config_dir(){
-            Some(dir) => {
-                config_file.push(dir);
-                config_file.push("oye");
-                if(tokio::fs::metadata(&config_file)).await.is_ok() == false{
-                    let _ = tokio::fs::create_dir_all(&config_file).await;
-                }
-                config_file.push("config.yml");
-                if tokio::fs::metadata(&config_file).await.is_ok() == false{
-                    Self::write(&config_file).await
-                }
-                return Self::read_content(&config_file).await;
-            }
-            None =>{
-                println!("Not exists config dir");
-                process::exit(1);
-            }
-
-        }
-    }
 
     pub fn get_log_level(&self) -> &str {
         &self.log_level
     }
 
-    pub async fn ask(&self, question: &str) -> Result<String, reqwest::Error>{
-        let url = format!("{}/{}", self.base_url, self.endpoint);
+    pub fn get_token(&self) -> &str{
+        &self.token
+    }
+
+    pub fn set_token(&mut self, token: String){
+        self.token = token;
+    }
+
+    pub async fn save(&self, path: &PathBuf){
+        let _ = tokio::fs::write(
+            path,
+            serde_yaml::to_string(&self).unwrap().as_bytes()
+        ).await;
+    }
+
+    pub async fn ask(&self, question: &str) -> Result<String, Box<dyn Error + 'static>>{
+        let url = format!("https://{}/{}", self.base_url, self.endpoint);
         debug!("Url: {}", url);
         debug!("Question: {}", question);
-        let client = reqwest::Client::new();
-        let response = client.post(&url)
+        let https = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .https_only()
+            .enable_http1()
+            .build();
+        let client = Client::builder().build(https);
+        let body = serde_json::to_string(&json!({
+            "model": self.model,
+            "messages": [
+                {"role": "system", "content": &self.content},
+                {"role": "user", "content": format!("'{}'", question)}
+            ]
+        })).unwrap();
+        let request = Request::builder()
+            .method("POST")
+            .uri(url)
             .header("Content-Type", "application/json")
             .header("Authorization", format!("Bearer {}", self.token))
-            .json(&json!({
-                "model": self.model,
-                "messages": [
-                    {"role": "system", "content": &self.content},
-                    {"role": "user", "content": format!("'{}'", question)}
-                ]}
-            ))
-            .send()
-            .await
-            .unwrap()
-            .json::<serde_json::Value>()
-            .await
+            .body(hyper::Body::from(body))
             .unwrap();
-        debug!("Response: {:#?}", response);
-        let command = response.get("choices")
-            .unwrap()
-            .as_array()
-            .unwrap()[0]
-            .get("message")
-            .unwrap()
-            .get("content")
-            .unwrap()
-            .as_str()
-            .unwrap();
-        // "choices": Array [
-        //     Object {
-        //         "finish_reason": String("stop"),
-        //         "index": Number(0),
-        //         "message": Object {
-        //             "content": String("ls"),
-        //             "role": String("assistant"),
-        //         },
-        //     },
-        // ],
-        // "created": Number(1678996561),
-        // "id": String("chatcmpl-6unzdarhMpabCQ6gegPPcriXIUSpn"),
-        // "model": String("gpt-3.5-turbo-0301"),
-        // "object": String("chat.completion"),
-        // "usage": Object {
-        //     "completion_tokens": Number(2),
-        //     "prompt_tokens": Number(96),
-        //     "total_tokens": Number(98),
-        // },
-        debug!("Command: {}", command);
-        Ok(command.to_string())
+        match client.request(request).await{
+            Ok(resp) => {
+                let body_bytes = hyper::body::to_bytes(resp.into_body()).await?;
+                // Convert the body bytes to utf-8
+                let body = String::from_utf8(body_bytes.to_vec()).unwrap();
+                debug!("{}", &body);
+                let data: serde_json::Value = serde_json::from_str(&body).unwrap();
+                if data.get("error").is_some(){
+                    let error = data.get("error").unwrap();
+                    let message = error.get("message").unwrap().as_str().unwrap();
+                    Err(Box::new(IABotError::new(message)))
+                }else{
+                    let command = data.get("choices")
+                        .unwrap()
+                        .as_array()
+                        .unwrap()[0]
+                        .get("message")
+                        .unwrap()
+                        .get("content")
+                        .unwrap()
+                        .as_str()
+                        .unwrap();
+                    Ok(command.to_string())
+                }
+            },
+            Err(e) => {
+                error!("{}", e);
+                Err(Box::new(e))
+            }
+        }
     }
+
     fn default() -> Self{
         Self{
             log_level: get_default_log_level(),
@@ -153,11 +177,15 @@ impl IABot{
             content: get_default_content(),
         }
     }
-    async fn write(file: &PathBuf){
+
+    pub async fn write_default(file: &PathBuf){
         let default = Self::default();
-        let _ = tokio::fs::write(file,serde_yaml::to_string(&default).unwrap().as_bytes()).await;
+        let _ = tokio::fs::write(
+            file,
+            serde_yaml::to_string(&default).unwrap().as_bytes()).await;
     }
-    async fn read_content(file: &PathBuf) -> IABot{
+
+    pub async fn read_content(file: &PathBuf) -> IABot{
         info!("File to read: {:?}", file);
         let content = match read_to_string(file)
             .await {
@@ -182,6 +210,5 @@ impl IABot{
             }
         }
     }
-
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
 mod ia;
 
 use ia::IABot;
-use inquire::Select;
+use inquire::{Select, Text};
 use spinners::{Spinner, Spinners};
 use std::{str::FromStr, env};
 use tracing_subscriber::{EnvFilter,
     layer::SubscriberExt, util::SubscriberInitExt};
 use tracing::{debug, info, error};
 use colored::Colorize;
+use std::{process, path::PathBuf};
 
 #[tokio::main]
 async fn main() {
-    let iabot = IABot::new().await;
+    let config = match get_config().await{
+        Some(path) => path,
+        None => {
+            let mut path = std::env::current_dir().unwrap();
+            path.push("oye.yml");
+            IABot::write_default(&path).await;
+            path
+        }
+    };
+    let mut iabot = IABot::read_content(&config).await;
+    while iabot.get_token() == ""{
+        let message = format!("Error: Token in {} is empty", &config.to_str().unwrap());
+        println!("{}", message.red());
+        let api_key = Text::new("Set the API key (https://platform.openai.com/account/api-keys):")
+            .prompt();
+        match api_key{
+            Ok(api_key) => {
+                debug!("Response: '{}'", &api_key);
+                if !api_key.is_empty() {
+                    iabot.set_token(api_key);
+                    iabot.save(&config).await;
+                    break;
+                }
+            },
+            Err(e) => {
+                println!("An error happened: {}", e);
+                process::exit(1);
+            }
+        }
+    }
     tracing_subscriber::registry()
         .with(EnvFilter::from_str(iabot.get_log_level()).unwrap())
         .with(tracing_subscriber::fmt::layer())
@@ -22,6 +52,8 @@ async fn main() {
     match iabot.ask(&question).await{
         Ok (command) => {
             spinner.stop();
+            info!("Command: {}", &command);
+            let command = command.replace("\"", "");
             info!("Command: {}", &command);
             let ans = Select::new(
                 &format!("¿Ejecuto {}?", &command.red()),
@@ -37,7 +69,11 @@ async fn main() {
                             command.args(&args[1..]);
                         }
                         debug!("Command: {:?}", command);
-                        let response = command.output().await.unwrap();
+                        let response = command
+                            .env("PWD", std::env::current_exe().unwrap())
+                            .output()
+                            .await
+                            .unwrap();
                         // let mut child = command.spawn().unwrap();
                         debug!("Response: {:?}", response);
                         // child.wait().await.unwrap();
@@ -66,6 +102,42 @@ async fn main() {
         Err(e) => {
             spinner.stop();
             error!("Error: {}", e);
+            println!("Error: {}", e.to_string().red());
         }
     }
+}
+
+async fn get_config() -> Option<PathBuf>{
+    let mut current_path = std::env::current_dir().unwrap();
+    current_path.push("oye.yml");
+    debug!("Current path: {}", current_path.display());
+    if(tokio::fs::metadata(&current_path)).await.is_ok(){
+        return Some(current_path);
+    }
+    let mut exe_path = std::env::current_exe().unwrap();
+    exe_path.push("oye.yml");
+    debug!("Exe path: {}", exe_path.display());
+    if(tokio::fs::metadata(&exe_path)).await.is_ok(){
+        return Some(exe_path);
+    }
+    let mut home_path = dirs::home_dir().unwrap();
+    debug!("Home path: {}", home_path.display());
+    home_path.push(".oye.yml");
+    if(tokio::fs::metadata(&home_path)).await.is_ok(){
+        return Some(home_path);
+    }
+    let mut config_path = dirs::config_dir().unwrap();
+    config_path.push("oye.yml");
+    debug!("Config path: {}", config_path.display());
+    if(tokio::fs::metadata(&config_path)).await.is_ok(){
+        return Some(config_path);
+    }
+    let mut config_folder = dirs::config_dir().unwrap();
+    config_folder.push("oye");
+    config_folder.push("config.yml");
+    debug!("Config folder: {}", config_folder.display());
+    if(tokio::fs::metadata(&config_folder)).await.is_ok(){
+        return Some(config_folder);
+    }
+    None
 }
